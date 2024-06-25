1c10cc6fa546a0258987a122c49112709e2832c1
Charlys Farias <charlesaugustoararaazul@gmail.com>
2024-06-24 23:03:50 -0300

    fix: fixing minor errors

--- backend/src/controllers/history.ts
+++ backend/src/controllers/history.ts
@@ -1,24 +1,21 @@
 import { Router } from 'express';
-import History from '../models/History';
+import History from '../models/history';
 
 const router = Router();
 
-// History Page
 router.get('/history', (req, res) => {
   res.render('history');
 });
 
-// View Song History
 router.get('/history/songs', async (req, res) => {
   const userId = req.query.userId as string;
-  const songsHistory = await History.find({ userId, itemType: 'song' }).sort('-listenedAt').exec();
+  const songsHistory = await History.find({ userId, itemType: 'música' }).sort('-data').exec();
   res.render('songHistory', { songsHistory });
 });
 
-// View Podcast History
 router.get('/history/podcasts', async (req, res) => {
   const userId = req.query.userId as string;
-  const podcastsHistory = await History.find({ userId, itemType: 'podcast' }).sort('-listenedAt').exec();
+  const podcastsHistory = await History.find({ userId, itemType: 'podcast' }).sort('-data').exec();
   res.render('podcastHistory', { podcastsHistory });
 });
 
