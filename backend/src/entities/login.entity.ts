dfdc11dccaabb32ef3bee67752efb814f56b60f1
Thiago de Souza Farias <tsf@cin.ufpe.br>
2024-07-21 17:22:52 -0300

    fix: returns user id on login

--- backend/src/entities/login.entity.ts
+++ backend/src/entities/login.entity.ts
@@ -1,9 +1,11 @@
 export default class LoginEntity{
   email: string;
   password: string;
+  id: string;
 
   constructor(data: Partial<LoginEntity>) {
     this.email = data.email || '';
     this.password = data.password || '';
+    this.id = data.id || '';
   }
 }
