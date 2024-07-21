5678078912cda6c853e308b163dbb8b171cbdef4
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-07-21 16:49:15 -0300

    feat: modelo inicial da pagina de playlists feito

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ToDoView from '../views/ToDoView.vue'
+import PlaylistsView from '@/views/PlaylistsView.vue'
+import MainPageView from '@/views/MainPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +8,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: ToDoView
+      component: MainPageView
     },
+
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/playlists',
+      name: "playlists",
+      component: PlaylistsView
+    },
+
   ]
 })
 
